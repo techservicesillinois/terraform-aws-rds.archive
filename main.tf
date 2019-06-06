18c32f9e67c7e13ed03e1815c2348222d7d24799
Jon R. Roma <roma@illinois.edu>
2019-06-06 18:16:31 -0500

    Code cleanup; add outputs

--- main.tf
+++ main.tf
@@ -13,42 +13,30 @@ data "aws_security_group" "selected" {
 }
 
 resource "aws_db_instance" "default" {
-  identifier = "${var.identifier}"
-
-  engine            = "${var.engine}"
-  engine_version    = "${var.engine_version}"
-  instance_class    = "${var.instance_class}"
-  allocated_storage = "${var.allocated_storage}"
-  storage_type      = "${var.storage_type}"
-  storage_encrypted = "${var.storage_encrypted}"
-  kms_key_id        = "${var.kms_key_id}"
-  license_model     = "${var.license_model}"
-
-  name     = "${var.name}"
-  username = "${var.username}"
-  password = "${var.password}"
-  port     = "${var.port}"
-
+  identifier                          = "${var.identifier}"
+  engine                              = "${var.engine}"
+  engine_version                      = "${var.engine_version}"
+  instance_class                      = "${var.instance_class}"
+  allocated_storage                   = "${var.allocated_storage}"
+  storage_type                        = "${var.storage_type}"
+  storage_encrypted                   = "${var.storage_encrypted}"
+  kms_key_id                          = "${var.kms_key_id}"
+  license_model                       = "${var.license_model}"
+  name                                = "${var.name}"
+  username                            = "${var.username}"
+  password                            = "${var.password}"
+  port                                = "${var.port}"
   iam_database_authentication_enabled = "${var.iam_database_authentication_enabled}"
   replicate_source_db                 = "${var.replicate_source_db}"
+  snapshot_identifier                 = "${var.snapshot_identifier}"
+  vpc_security_group_ids              = ["${data.aws_security_group.selected.*.id}"]
+  availability_zone                   = "${var.availability_zone}"
+  multi_az                            = "${var.multi_az}"
+  iops                                = "${var.iops}"
+  publicly_accessible                 = "${var.publicly_accessible}"
+  monitoring_interval                 = "${var.monitoring_interval}"
 
-  # snapshot_identifier                 = "${local.snapshot_identifier}"
-  snapshot_identifier = "${var.snapshot_identifier}"
-
-  ### Network config block
-  vpc_security_group_ids = ["${data.aws_security_group.selected.*.id}"]
-
-  ####
-
-  availability_zone   = "${var.availability_zone}"
-  multi_az            = "${var.multi_az}"
-  iops                = "${var.iops}"
-  publicly_accessible = "${var.publicly_accessible}"
-  monitoring_interval = "${var.monitoring_interval}"
-
-  ### Is this Iam below?
   # monitoring_role_arn = "${coalesce(var.monitoring_role_arn, join("", aws_iam_role.enhanced_monitoring.*.arn))}"
-
   allow_major_version_upgrade     = "${var.allow_major_version_upgrade}"
   auto_minor_version_upgrade      = "${var.auto_minor_version_upgrade}"
   apply_immediately               = "${var.apply_immediately}"
@@ -63,10 +51,9 @@ resource "aws_db_instance" "default" {
   timeouts                        = "${var.timeouts}"
   deletion_protection             = "${var.deletion_protection}"
   tags                            = "${merge(map("Name", var.identifier), var.tags)}"
-  ### Inline resources
-  db_subnet_group_name = "${var.db_subnet_group_name}"
-  parameter_group_name = "${var.parameter_group_name}"
-  option_group_name    = "${var.option_group_name}"
+  db_subnet_group_name            = "${var.db_subnet_group_name}"
+  parameter_group_name            = "${var.parameter_group_name}"
+  option_group_name               = "${var.option_group_name}"
 }
 
 #resource "aws_db_option_group" "default" {
